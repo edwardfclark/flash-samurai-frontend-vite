4442663ada76075b179c3a0e1ac256c88ca665c9
Edward Clark <edwardfclark@gmail.com>
2024-02-27 12:56:33 +0000

    Fix the youtube link format in ReferencesList

--- src/pages/GroupQuiz/ReferencesList.tsx
+++ src/pages/GroupQuiz/ReferencesList.tsx
@@ -1,12 +1,11 @@
 import { IReference } from "../../types/Cards";
-import { Stack, Typography, Link } from "@mui/material";
+import { Stack, Typography, Link, Box } from "@mui/material";
 
-function formatYoutubeLink(url?: string, timestampSeconds?: string) {
+function createYouTubeLink(videoID?: string, timestampSeconds?: string) {
+  const url = `https://www.youtube.com/watch?v=${videoID}`;
   // If no timestamp, return the url as is
+  // Otherwise add a timestamp
   if (!timestampSeconds) return url;
-
-  // If there are already query params, append the timestamp with &. Otherwise, use ?
-  if (!url?.includes("?")) return `${url}?t=${timestampSeconds}`;
   return `${url}&t=${timestampSeconds}`;
 }
 
@@ -18,25 +17,22 @@ export function ReferencesList({ references }: { references: IReference[] }) {
       <Typography variant="h6">References:</Typography>
       <Stack>
         {references.map((ref, idx) => (
-          <>
-            {ref.type === "text" && (
-              <Typography key={ref?._id || idx}>{ref.text}</Typography>
-            )}
+          <Box key={idx}>
+            {ref.type === "text" && <Typography>{ref.text}</Typography>}
             {ref.type === "link" && Boolean(ref?.url) && (
-              <Link href={ref?.url} key={ref?._id || idx} target="_blank">
+              <Link href={ref?.url} target="_blank">
                 {ref?.text || ref?.url}
               </Link>
             )}
-            {ref.type === "youtube" && Boolean(ref?.url) && (
+            {ref.type === "youtube" && Boolean(ref?.videoID) && (
               <Link
-                href={formatYoutubeLink(ref?.url, ref?.timestampSeconds)}
-                key={ref?._id || idx}
+                href={createYouTubeLink(ref?.videoID, ref?.timestampSeconds)}
                 target="_blank"
               >
                 {ref?.text || ref?.url}
               </Link>
             )}
-          </>
+          </Box>
         ))}
       </Stack>
     </>
